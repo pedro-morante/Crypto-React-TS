f0520a1f44bc1940c2abda42d75ccdb499110fd0
Pedro Morante <pedro.morante@inchcape.com>
2024-10-15 09:23:36 -0500

    add Spinner component

--- src/store.ts
+++ src/store.ts
@@ -6,6 +6,7 @@ import { fetchCurrentCryptoPrice, getCryptos } from "./services/CryptoService";
 type CryptoStore = {
   cryptocurrencies: CryptoCurrency[];
   result: CryptoPrice;
+  loading: boolean;
   fetchCryptos: () => Promise<void>;
   fetchData: (pair: Pair) => Promise<void>;
 };
@@ -14,6 +15,7 @@ export const useCryptoStore = create<CryptoStore>()(
   devtools((set) => ({
     cryptocurrencies: [],
     result: {} as CryptoPrice,
+    loading: false,
     fetchCryptos: async () => {
       const cryptocurrencies = await getCryptos();
       set(() => ({
@@ -21,10 +23,15 @@ export const useCryptoStore = create<CryptoStore>()(
       }));
     },
     fetchData: async (pair) => {
+      set(() => ({
+        loading: true,
+      }));
+
       const result = await fetchCurrentCryptoPrice(pair);
 
       set(() => ({
         result,
+        loading: false,
       }));
     },
   }))
